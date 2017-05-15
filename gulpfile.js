fbfb9fed97e457d44ab9da00b8eac5f1dba926c3
faychou <fay_chou@163.com>
2017-05-15 15:40:17 +0800

    设置静态服务器

--- gulpfile.js
+++ gulpfile.js
@@ -9,41 +9,49 @@ var gulp = require("gulp"),
 gulp.task('minify-html', function () {
 	return gulp.src('src/*.html')
 	.pipe(minifyHtml())
-	.pipe(gulp.dest('dist'));
+	.pipe(gulp.dest('dist'))
+	.pipe(browserSync.reload({stream:true}));
 });
 
 
 gulp.task('minify-css', function() {
 	return gulp.src('src/css/*.css')
 		.pipe(cleanCSS({compatibility: 'ie8'}))
-		.pipe(gulp.dest('dist/css'));
+		.pipe(gulp.dest('dist/css'))
+		.pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task('jsLint', function () {
     gulp.src('src/js/*.js')
     .pipe(jshint())
-    .pipe(jshint.reporter());
+    .pipe(jshint.reporter())
+    .pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task('minify-js', function () {
 	return gulp.src('src/js/*.js')
 	.pipe(uglify())
-	.pipe(gulp.dest('dist/js'));
+	.pipe(gulp.dest('dist/js'))
+	.pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task('clean-file', function () {
 	return gulp.src('dist', {read: false})
-		.pipe(clean());
+		.pipe(clean())
+		.pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task("default",["clean-file","minify-html","minify-css","jsLint","minify-js"]);
 
-/*gulp.task('serve', ['clean-file'], function() {
+gulp.task('serve', ["clean-file","minify-html","minify-css","jsLint","minify-js"], function() {
 	browserSync.init({
-		server: "./app"
+		server: {
+			baseDir: "./"
+		}
 	});
 
-	gulp.watch("app/scss/*.scss", ['sass']);
-	gulp.watch("app/*.html").on('change', browserSync.reload;);
-});*/
+	gulp.watch("src/css/*.css", ['minify-css']);
+	gulp.watch("src/js/*.js", ['jsLint','minify-js']);
+	gulp.watch("src/*.html").on('change', browserSync.reload);
+});
 
